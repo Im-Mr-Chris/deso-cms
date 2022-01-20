ab82b60e5337ccc083f4b4d9ed55c81e972e16ad
Christopher Celaya <im-mr-chris@protonmail.ch>
2022-01-20 17:46:31 -0600

    update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,54 +1,39 @@
 import React from "react";
 
-import { User as FirebaseUser } from "firebase/auth";
-import {
-    Authenticator,
-    buildCollection,
-    buildProperty,
-    buildSchema,
-    EntityReference,
-    FirebaseCMSApp,
-    NavigationBuilder,
-    NavigationBuilderProps
-} from "@camberi/firecms";
+import { GoogleAuthProvider } from "firebase/auth";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "typeface-rubik";
 import "typeface-space-mono";
 
-// TODO: Replace with your config
-const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-};
+import {
+    AuthDelegate,
+    Authenticator,
+    buildCollection,
+    buildSchema,
+    CircularProgressCenter,
+    createCMSDefaultTheme,
+    FirebaseLoginView,
+    FireCMS,
+    NavigationBuilder,
+    NavigationBuilderProps,
+    NavigationRoutes,
+    Scaffold,
+    SideEntityDialogs,
+    useFirebaseAuthDelegate,
+    useFirebaseStorageSource,
+    useFirestoreDataSource,
+    useInitialiseFirebase
+} from "@camberi/firecms";
 
-const locales = {
-    "en-US": "English (United States)",
-    "es-ES": "Spanish (Spain)",
-    "de-DE": "German"
-};
+import { firebaseConfig } from "./firebase_config";
 
-type Product = {
-    name: string;
-    price: number;
-    status: string;
-    published: boolean;
-    related_products: EntityReference[];
-    main_image: string;
-    tags: string[];
-    description: string;
-    categories: string[];
-    publisher: {
-        name: string;
-        external_id: string;
-    },
-    expires_on: Date
-}
+const DEFAULT_SIGN_IN_OPTIONS = [
+    GoogleAuthProvider.PROVIDER_ID
+];
 
-const productSchema = buildSchema<Product>({
+const productSchema = buildSchema({
     name: "Product",
     properties: {
         name: {
@@ -79,178 +64,112 @@ const productSchema = buildSchema<Product>({
                     public: "Public"
                 }
             }
-        },
-        published: ({ values }) => buildProperty({
-            title: "Published",
-            dataType: "boolean",
-            columnWidth: 100,
-            disabled: (
-                values.status === "public"
-                    ? false
-                    : {
-                        clearOnDisabled: true,
-                        disabledMessage: "Status must be public in order to enable this the published flag"
-                    }
-            )
-        }),
-        related_products: {
-            dataType: "array",
-            title: "Related products",
-            description: "Reference to self",
-            of: {
-                dataType: "reference",
-                path: "products"
-            }
-        },
-        main_image: buildProperty({ // The `buildProperty` method is an utility function used for type checking
-            title: "Image",
-            dataType: "string",
-            config: {
-                storageMeta: {
-                    mediaType: "image",
-                    storagePath: "images",
-                    acceptedFiles: ["image/*"]
-                }
-            }
-        }),
-        tags: {
-            title: "Tags",
-            description: "Example of generic array",
-            validation: { required: true },
-            dataType: "array",
-            of: {
-                dataType: "string"
-            }
-        },
-        description: {
-            title: "Description",
-            description: "Not mandatory but it'd be awesome if you filled this up",
-            longDescription: "Example of a long description hidden under a tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis bibendum turpis. Sed scelerisque ligula nec nisi pellentesque, eget viverra lorem facilisis. Praesent a lectus ac ipsum tincidunt posuere vitae non risus. In eu feugiat massa. Sed eu est non velit facilisis facilisis vitae eget ante. Nunc ut malesuada erat. Nullam sagittis bibendum porta. Maecenas vitae interdum sapien, ut aliquet risus. Donec aliquet, turpis finibus aliquet bibendum, tellus dui porttitor quam, quis pellentesque tellus libero non urna. Vestibulum maximus pharetra congue. Suspendisse aliquam congue quam, sed bibendum turpis. Aliquam eu enim ligula. Nam vel magna ut urna cursus sagittis. Suspendisse a nisi ac justo ornare tempor vel eu eros.",
-            dataType: "string",
-            columnWidth: 300
-        },
-        categories: {
-            title: "Categories",
-            validation: { required: true },
-            dataType: "array",
-            of: {
-                dataType: "string",
-                config: {
-                    enumValues: {
-                        electronics: "Electronics",
-                        books: "Books",
-                        furniture: "Furniture",
-                        clothing: "Clothing",
-                        food: "Food"
-                    }
-                }
-            }
-        },
-        publisher: {
-            title: "Publisher",
-            description: "This is an example of a map property",
-            dataType: "map",
-            properties: {
-                name: {
-                    title: "Name",
-                    dataType: "string"
-                },
-                external_id: {
-                    title: "External id",
-                    dataType: "string"
-                }
-            }
-        },
-        expires_on: {
-            title: "Expires on",
-            dataType: "timestamp"
         }
     }
 });
 
-const localeSchema = buildSchema({
-    customId: locales,
-    name: "Locale",
-    properties: {
-        title: {
-            title: "Title",
-            validation: { required: true },
-            dataType: "string"
-        },
-        selectable: {
-            title: "Selectable",
-            description: "Is this locale selectable",
-            dataType: "boolean"
-        },
-        video: {
-            title: "Video",
-            dataType: "string",
-            validation: { required: false },
-            config: {
-                storageMeta: {
-                    mediaType: "video",
-                    storagePath: "videos",
-                    acceptedFiles: ["video/*"]
-                }
-            }
-        }
-    }
-});
+/**
+ * This is an example of how to use the components provided by FireCMS for
+ * a better customisation.
+ */
+export function CustomCMSApp() {
 
-export default function App() {
-
-    const navigation: NavigationBuilder = async ({
-                                                     user,
-                                                     authController
-                                                 }: NavigationBuilderProps) => {
-
-        return ({
-            collections: [
-                buildCollection({
-                    path: "products",
-                    schema: productSchema,
-                    name: "Products",
-                    permissions: ({ authController }) => ({
-                        edit: true,
-                        create: true,
-                        // we have created the roles object in the navigation builder
-                        delete: authController.extra.roles.includes("admin")
-                    }),
-                    subcollections: [
-                        buildCollection({
-                            name: "Locales",
-                            path: "locales",
-                            schema: localeSchema
-                        })
-                    ]
+    const navigation: NavigationBuilder = ({ user }: NavigationBuilderProps) => ({
+        collections: [
+            buildCollection({
+                path: "products",
+                schema: productSchema,
+                name: "Products",
+                permissions: ({ user }) => ({
+                    edit: true,
+                    create: true,
+                    delete: true
                 })
-            ]
-        });
-    };
+            })
+        ]
+    });
 
-    const myAuthenticator: Authenticator<FirebaseUser> = async ({
-                                                                    user,
-                                                                    authController
-                                                                }) => {
-        // You can throw an error to display a message
-        if(user?.email?.includes("flanders")){
-            throw Error("Stupid Flanders!");
-        }
-        
+    const myAuthenticator: Authenticator = ({ user }) => {
         console.log("Allowing access to", user?.email);
-        // This is an example of retrieving async data related to the user
-        // and storing it in the user extra field.
-        const sampleUserData = await Promise.resolve({
-            roles: ["admin"]
-        });
-        authController.setExtra(sampleUserData);
         return true;
     };
 
-    return <FirebaseCMSApp
-        name={"My Online Shop"}
-        authentication={myAuthenticator}
-        navigation={navigation}
-        firebaseConfig={firebaseConfig}
-    />;
+    const {
+        firebaseApp,
+        firebaseConfigLoading,
+        configError,
+        firebaseConfigError
+    } = useInitialiseFirebase({ firebaseConfig });
+
+    const authDelegate: AuthDelegate = useFirebaseAuthDelegate({
+        firebaseApp,
+    });
+
+    const dataSource = useFirestoreDataSource({
+        firebaseApp: firebaseApp
+        // You can add your `FirestoreTextSearchController` here
+    });
+    const storageSource = useFirebaseStorageSource({ firebaseApp: firebaseApp });
+
+    if (configError) {
+        return <div> {configError} </div>;
+    }
+
+    if (firebaseConfigError) {
+        return <div>
+            It seems like the provided Firebase config is not correct. If you
+            are using the credentials provided automatically by Firebase
+            Hosting, make sure you link your Firebase app to Firebase
+            Hosting.
+        </div>;
+    }
+
+    if (firebaseConfigLoading || !firebaseApp) {
+        return <CircularProgressCenter/>;
+    }
+
+    return (
+        <Router>
+            <FireCMS navigation={navigation}
+                     authentication={myAuthenticator}
+                     authDelegate={authDelegate}
+                     dataSource={dataSource}
+                     storageSource={storageSource}
+                     entityLinkBuilder={({ entity }) => `https://console.firebase.google.com/project/${firebaseApp.options.projectId}/firestore/data/${entity.path}/${entity.id}`}
+            >
+                {({ context, mode, loading }) => {
+
+                    const theme = createCMSDefaultTheme({ mode });
+
+                    let component;
+                    if (loading) {
+                        component = <CircularProgressCenter/>;
+                    } else if (!context.authController.canAccessMainView) {
+                        component = (
+                            <FirebaseLoginView
+                                skipLoginButtonEnabled={false}
+                                signInOptions={DEFAULT_SIGN_IN_OPTIONS}
+                                firebaseApp={firebaseApp}
+                                authDelegate={authDelegate}/>
+                        );
+                    } else {
+                        component = (
+                            <Scaffold name={"My Online Shop"}>
+                                <NavigationRoutes/>
+                                <SideEntityDialogs/>
+                            </Scaffold>
+                        );
+                    }
+
+                    return (
+                        <ThemeProvider theme={theme}>
+                            <CssBaseline/>
+                            {component}
+                        </ThemeProvider>
+                    );
+                }}
+            </FireCMS>
+        </Router>
+    );
+
 }
